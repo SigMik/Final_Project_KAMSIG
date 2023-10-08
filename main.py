73c3a5d0c0aa8cbef2338c749ec327b7af55d87d
kamilako9 <kamilee.ko@gmail.com>
2023-10-08 11:34:03 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-Welcome to our final project :)
-One more welcome for check :)
-Now I see you ;)
+from bs4 import BeautifulSoup
+import requests
+
+
+url = 'https://lkl.lt/statistika'
+
+response = requests.get(url)
+
+print(response.status_code)
+soup = BeautifulSoup(response.content, 'html.parser')
+#imame visus elementus is bloko, kuriame yra zaideju info
+zaideju_info = soup.find_all('div', class_='mb-8')
+print(zaideju_info)
