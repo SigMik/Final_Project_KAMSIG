e4151b421f55af60e87dd7d2c3e59403ee2b6d58
SigMik <smikolainiene@gmail.com>
2023-10-09 18:18:06 +0300

    commit message

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
+data=[] #susikuriame sarasa, i kuri desime zaidejus
 
-url = 'https://lkl.lt/statistika'
+url = f'https://en.lkl.lt/get-players-stats?category=points&tab=sums&season_id=30527&additional_filters=0&team_id=-&month=&search_text='
 
 response = requests.get(url)
-
 print(response.status_code)
+
 soup = BeautifulSoup(response.content, 'html.parser')
-#imame visus elementus is bloko, kuriame yra zaideju info
-zaideju_info = soup.find_all('div', class_='mb-8')
-print(zaideju_info)
+players = soup.find_all('div', class_='mb-8')
+# print(players)
+
+
+for player in players: #susikuriame kintamuosius
+    name= player.find('td', class_='name pr-1').text.strip()
+    team = player.find('td', class_='pr-1').text.strip()
+    games = player.find('td', class_='pr-1').text.strip()
+    sum = player.find('td', class_='pr-1' ).text.strip()
+    data.append((name, team, games, sum))
+
+df=pd.DataFrame(data, columns=['Name', 'Team', 'Games', 'Sum'])
+print(df)
