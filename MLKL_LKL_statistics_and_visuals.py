bd96cc78534723815bde9f23718ed43a5fa1473c
SigMik <smikolainiene@gmail.com>
2023-10-11 22:04:24 +0300

    commit message

--- MLKL_LKL_statistics_and_visuals.py
+++ MLKL_LKL_statistics_and_visuals.py
@@ -2,10 +2,46 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# daugiausiai taškų pelnęs žaidėjas LKL ir MLKL
+# didziausią taškų vidurkį per sezono rungtynes pelnęs žaidėjas LKL ir MLKL
 df = pd.read_csv('LKL_points.csv')
 df1 = pd.read_csv('MLKL_points.csv')
 
-LKL_avg_point_per_games = df['Player'].['Sum'].max()
-print(LKL_avg_point_per_games)
+df['Result']=df['Sum']/df['Games']
+sortdf=df.sort_values(by='Result', ascending=False)
+# print(sortdf)
+
+df1['Result']=df1['Sum']/df['Games']
+sortdf1=df1.sort_values(by='Result', ascending=False)
+print(sortdf1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
